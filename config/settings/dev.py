50d4a53873cecd10d0412f1fe45006c5ac207113
davecaputo <davecaputo@hotmail.com>
2018-02-14 08:56:11 +0000

    First commit with basic home page setup with fullPage js.

---
+++ config/settings/dev.py
@@ -0,0 +1,30 @@
+from __future__ import absolute_import, unicode_literals
+
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# Postgres database configuration..
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'rgsite',
+        'USER': 'rgadmin',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'h6(wigfx9ca@8_460(rr3hu^sk&%w)=3ut@e(e$2_(#)*o94)q'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
