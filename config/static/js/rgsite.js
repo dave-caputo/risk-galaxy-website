854c6133b34b5ba228f9c687202a0c8342ec2524
davecaputo <davecaputo@hotmail.com>
2018-03-08 14:46:19 +0000

    Slides initialized.

--- config/static/js/rgsite.js
+++ config/static/js/rgsite.js
@@ -1,9 +1,29 @@
 $(document).ready(function() {
 
     $('#fullpage').fullpage({
+        afterLoad: function(anchorLink, index) {
+
+            if (index != 1) {
+                $('.navbar-brand').removeClass('d-none').fadeIn(600);
+            }
+
+        },
         anchors: ['home', 'product', 'zen', 'investors', 'about'],
         menu: '.navbar-nav',
-        navigation: true,
-        verticalCentered: false
+        // navigation: true,
+        onLeave: function(index, nextIndex, direction) {
+            var leavingSection = $(this);
+
+            //after leaving section 2
+            if (nextIndex == 1) {
+                $('.navbar-brand').fadeOut(200).addClass('d-none');
+
+            }
+        },
+        navigationTooltips: ['firstSlide', 'secondSlide'],
+        // sectionsColor: ['blue', 'red', 'yellow'],
+        verticalCentered: true,
+        slidesNavigation: false,
+        slidesNavPosition: 'bottom',
     });
 });
